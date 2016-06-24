616163cd7d2b10e3785b8b6cd59df42fab340baf
502647092 <admin@yumc.pw>
2016-06-24 18:35:27 +0800

    feat: 添加管理 数据 监听
    Signed-off-by: 502647092 <admin@yumc.pw>

---
+++ src/main/java/pw/yumc/MiaoBoard/listener/PlayerListener.java
@@ -0,0 +1,20 @@
+package pw.yumc.MiaoBoard.listener;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+/**
+ *
+ * @since 2016年6月24日 下午3:29:39
+ * @author 喵♂呜
+ */
+public class PlayerListener implements Listener {
+    public void onPlayerJoin(final PlayerJoinEvent e) {
+
+    }
+
+    public void onPlayerQuit(final PlayerQuitEvent e) {
+
+    }
+}
---
+++ src/main/java/pw/yumc/MiaoBoard/scoreboard/ScoreBoardManager.java
@@ -0,0 +1,10 @@
+package pw.yumc.MiaoBoard.scoreboard;
+
+/**
+ *
+ * @since 2016年6月24日 下午3:31:31
+ * @author 喵♂呜
+ */
+public class ScoreBoardManager {
+
+}
